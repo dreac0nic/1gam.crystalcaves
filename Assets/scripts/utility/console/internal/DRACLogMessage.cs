0ef8c3329aa3ba6fa551066c1eecc5107ee85258
SpenSer Bray <matthewqa@gmail.com>
2015-09-05 00:25:55 -0700

    Add simple logging message for console.

---
+++ Assets/scripts/utility/console/internal/DRACLogMessage.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+﻿using UnityEngine;
+
+namespace DRAConsole
+{
+	public class LogMessage : BaseMessage
+	{
+		public enum LogLevel { ERROR, WARNING, INFO };
+
+		public int Code { get { return m_Code; } }
+		public LogLevel Level { get { return m_Level; } }
+		public string LogLevelName { get { return LogMessage.LogLevelToString(m_Level); } }
+
+		protected int m_Code;
+		protected LogLevel m_Level;
+
+		public LogMessage(string contents, LogLevel level = LogLevel.INFO, int code = -1) : base(contents)
+		{
+			m_Level = level;
+			m_Code = code;
+		}
+
+		public override string ToString()
+		{
+			return base.ToString(string.Format("[{0}]{1}: ", LogMessage.LogLevelToString(m_Level), (m_Code >= 0 ? string.Format(" ({0})", m_Code) : "")));
+		}
+
+		protected static string LogLevelToString(LogLevel level)
+		{
+			switch(level) {
+				case LogLevel.ERROR: return "ERROR";
+				case LogLevel.WARNING: return "WARNING";
+				case LogLevel.INFO: return "INFO";
+				default: return "UNKNOWN";
+			}
+		}
+	}
+}
