f38b7d3e34996234841afadf431061c8492768f2
SpenSer Bray <matthewqa@gmail.com>
2015-09-09 15:35:40 -0700

    Build new interactables with specific purposes.

---
+++ Assets/scripts/player/interaction/InteractableOnTrigger.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractableOnTrigger : Interactable
+{
+	public bool TriggerEnter = false;
+	public bool TriggerStay = false;
+	public bool TriggerExit = false;
+
+	public virtual void OnTriggerEnter(Collider offender_collider)
+	{
+		if(TriggerEnter) {
+			processInteraction(processOffender(offender_collider), FireTargetBase.TriggerType.ENTER);
+		}
+	}
+
+	public virtual void OnTriggerStay(Collider offender_collider)
+	{
+		if(TriggerStay) {
+			processInteraction(processOffender(offender_collider), FireTargetBase.TriggerType.STAY);
+		}
+	}
+
+	public virtual void OnTriggerExit(Collider offender_collider)
+	{
+		if(TriggerExit) {
+			processInteraction(processOffender(offender_collider), FireTargetBase.TriggerType.EXIT);
+		}
+	}
+
+	protected virtual GameObject processOffender(Collider offender_collider)
+	{
+		return offender_collider.gameObject;
+	}
+}
---
+++ Assets/scripts/player/interaction/InteractableUsable.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InteractableUsable : Interactable
+{
+	public bool AllowAnonymouseTriggering = true;
+
+	public void Trigger(GameObject offender = null)
+	{
+		if(AllowAnonymouseTriggering || offender) {
+			processInteraction(offender);
+		}
+	}
+}
