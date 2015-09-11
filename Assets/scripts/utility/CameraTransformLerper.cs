5a51af9e1c8acfdc9f0cce7b96eaea193c42fce9
SpenSer Bray <matthewqa@gmail.com>
2015-09-10 17:06:28 -0700

    Create a CameraTransformLerper to keep the camera attached without childing.

---
+++ Assets/scripts/utility/CameraTransformLerper.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTransformLerper : MonoBehaviour
+{
+	public Transform CameraTarget;
+	public bool LerpPosition = true;
+	public bool SlerpRotation = true;
+	public float PositionSpeed = 5.0f;
+	public float RotationSpeed = 5.0f;
+
+	public void Update()
+	{
+		if(CameraTarget) {
+			if(LerpPosition) {
+				this.transform.position = Vector3.Lerp(this.transform.position, CameraTarget.position, PositionSpeed*Time.deltaTime);
+			} else {
+				this.transform.position = CameraTarget.position;
+			}
+
+			if(SlerpRotation) {
+				this.transform.localRotation = Quaternion.Slerp(this.transform.localRotation, CameraTarget.rotation, RotationSpeed*Time.deltaTime);
+			} else {
+				this.transform.localRotation = CameraTarget.rotation;
+			}
+		}
+	}
+}
