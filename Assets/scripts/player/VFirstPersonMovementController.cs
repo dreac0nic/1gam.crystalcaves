96f5003033e1b2210e1fa8533d1ad2dd9d2d8be6
SpenSer Bray <matthewqa@gmail.com>
2015-09-12 16:06:23 -0700

    Add jump and ghosting functions to movement controller

--- Assets/scripts/player/VFirstPersonMovementController.cs
+++ Assets/scripts/player/VFirstPersonMovementController.cs
@@ -19,16 +19,18 @@ public class VFirstPersonMovementController : MonoBehaviour
 	[Header("Movement")]
 	public float MaximumVelocity = 15.0f;
 	public float Acceleration = 20.0f;
+	public float JumpForce = 10.0f;
+	public float JumpLandingCooldown = 0.1f;
+
+	[Header("Jump Ghosting")]
+	public bool EnableGhosting = false;
+	public float GhostingDuration = 0.5f;
 
 	[Header("Ground Check")]
 	public Transform GroundCheckStart;
 	public float GroundCheckDistance = 0.01f;
 	public float GroundCheckRadius = 0.5f;
 	public Vector3 GroundCheckDirection = Vector3.down;
-
-	[Header("Ground Check Ghost")]
-	public bool EnableGhosting = false;
-	public float GhostingDuration = 0.5f;
 
 	[Header("Input Names")]
 	public string MovementAxis = "Vertical";
@@ -37,6 +39,7 @@ public class VFirstPersonMovementController : MonoBehaviour
 	public string MouseYAxis = "Mouse Y";
 	public string ControllerLookX = "Look Horizontal";
 	public string ControllerLookY = "Look Vertical";
+	public string Jump = "Jump";
 
 	// External references for player components
 	protected Rigidbody m_Rigidbody;
@@ -44,6 +47,10 @@ public class VFirstPersonMovementController : MonoBehaviour
 
 	// State Variables
 	protected bool m_IsGrounded = false;
+	protected bool m_JumpInput = false;
+	protected bool m_Jumped = false;
+	protected float m_LandingTimeout;
+	protected float m_GhostingTimeout;
 	protected Vector2 m_MovementInput;
 	protected Vector3 m_GroundNormal;
 
@@ -67,6 +74,8 @@ public class VFirstPersonMovementController : MonoBehaviour
 
 	public void Update()
 	{
+		// Update state inputs
+		m_JumpInput = Input.GetButton(Jump) && (Time.time >= m_LandingTimeout);
 		m_MovementInput = new Vector2(Input.GetAxisRaw(StrafingAxis), Input.GetAxisRaw(MovementAxis));
 		Vector3 rotational_input = new Vector2(Input.GetAxisRaw(MouseXAxis), Input.GetAxisRaw(MouseYAxis));
 
@@ -108,31 +117,46 @@ public class VFirstPersonMovementController : MonoBehaviour
 	public void FixedUpdate()
 	{
 		RaycastHit hit_info;
-		//Vector3 local_forward = (CameraAnchor ? CameraAnchor.forward : this.transform.forward);
-		//Vector3 local_right = (CameraAnchor ? CameraAnchor.right : this.transform.right);
+		Vector3 look_forward = (CameraAnchor ? CameraAnchor.forward : this.transform.forward);
+		Vector3 look_right = (CameraAnchor ? CameraAnchor.right : this.transform.right);
 
 		// Perform ground check to see if we have left the ground.
-		if(Physics.SphereCast(GroundCheckStart.position, GroundCheckRadius, GroundCheckDirection, out hit_info, (0.5f*m_Collider.height - m_Collider.radius) + GroundCheckDistance)) {
+		if(Physics.SphereCast(GroundCheckStart.position, GroundCheckRadius, GroundCheckDirection, out hit_info, (0.5f*m_Collider.height - GroundCheckRadius) + GroundCheckDistance)) {
 			if(!m_IsGrounded) {
 				m_IsGrounded = true;
+				m_LandingTimeout = Time.time + JumpLandingCooldown;
 			}
 
+			m_Jumped = false;
 			m_GroundNormal = hit_info.normal;
 		} else if(m_IsGrounded) {
 			m_IsGrounded = false;
 			m_GroundNormal = Vector3.up;
+			m_GhostingTimeout = Time.time + GhostingDuration;
+		}
+
+		// Jump!
+		if(m_JumpInput && !m_Jumped && (m_IsGrounded || (EnableGhosting && Time.time < m_GhostingTimeout))) {
+			m_Jumped = true;
+
+			// XXX: If we're in air due to ghosting, cancel out the downwards force for a single frame.
+			if(!m_IsGrounded) {
+				Vector3 rb_velocity = m_Rigidbody.velocity;
+				rb_velocity.y = 0.0f;
+
+				m_Rigidbody.velocity = rb_velocity;
+			}
+
+			m_Rigidbody.AddForce(JumpForce*Vector3.up, ForceMode.Impulse);
 		}
 
 		// Calculate movement based on current inputs
-		if(m_MovementInput.magnitude > float.Epsilon) {
+		if(m_MovementInput.magnitude > float.Epsilon && m_IsGrounded) {
 			float time_acceleration = Acceleration*Time.deltaTime;
-			Vector3 movement_input = new Vector3(m_MovementInput.x, 0.0f, m_MovementInput.y);
+			Vector3 movement_input = (m_MovementInput.y*look_forward + m_MovementInput.x*look_right);
+			movement_input = Vector3.ClampMagnitude(movement_input, 1.0f);
 
-			if(movement_input.magnitude > 1.0f) {
-				movement_input.Normalize();
-			}
-
-			Vector3 target_velocity = MaximumVelocity*this.transform.TransformDirection(movement_input);
+			Vector3 target_velocity = MaximumVelocity*Vector3.ProjectOnPlane(movement_input, m_GroundNormal).normalized;
 			Vector3 velocity_delta = target_velocity - m_Rigidbody.velocity;
 
 			velocity_delta = Mathf.Clamp(velocity_delta.magnitude, -time_acceleration, time_acceleration)*velocity_delta.normalized;
