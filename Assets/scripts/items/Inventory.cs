efd12b3ceac4e3877fb4c16b50a92292acdc19bb
SpenSer Bray <matthewqa@gmail.com>
2015-09-26 13:32:50 -0700

    Build a simple inventory pickup/drop system.

--- Assets/scripts/items/Inventory.cs
+++ Assets/scripts/items/Inventory.cs
@@ -10,13 +10,35 @@ public class Inventory : MonoBehaviour
 		public string Name = "New Slot";
 		public bool LimitItemCount = false;
 		public uint MaximumItems = 2;
-		public List<Transform> MountingAnchors;
-		public List<Item> Items;
+		[SerializeField] protected List<Transform> MountingAnchors;
+		[SerializeField] protected List<Item> Items;
 
 		public InventorySlot()
 		{
 			Items = new List<Item>();
 			MountingAnchors = new List<Transform>();
+		}
+
+		public bool Pickup(Item new_item)
+		{
+			if(!Items.Contains(new_item) && (!LimitItemCount || Items.Count < MaximumItems)) {
+				Items.Add(new_item);
+
+				return true;
+			}
+
+			return false;
+		}
+
+		public bool Drop(Item drop_item)
+		{
+			if(Items.Contains(drop_item)) {
+				Items.Remove(drop_item);
+
+				return true;
+			}
+
+			return false;
 		}
 
 		public Transform GetFreeMountingAnchor()
@@ -34,22 +56,34 @@ public class Inventory : MonoBehaviour
 	public bool Pickup(Item new_item)
 	{
 		InventorySlot slot;
-		if(this.retrieveSlot(new_item.Slot, out slot) && !slot.Items.Contains(new_item)) {
-			slot.Items.Add(new_item);
+		if(this.retrieveSlot(new_item.Slot, out slot)) { // XXX: Collapse, maybe...
+			if(!new_item.hasOwner || new_item.Owner.Drop(new_item)) {
+				if(slot.Pickup(new_item)) {
+					new_item.Owner = this;
+					new_item.transform.SetParent(this.transform, false);
+					new_item.transform.localPosition = Vector3.zero;
+					new_item.transform.localRotation = Quaternion.identity;
 
-			return true;
+					return true;
+				}
+			}
 		}
 
 		return false;
 	}
 
-	public bool Drop(Item drop_item)
+	public bool Drop(Item drop_item, bool being_stolen = false)
 	{
 		InventorySlot slot;
-		if(this.retrieveSlot(drop_item.Slot, out slot) && slot.Items.Contains(drop_item)) {
-			slot.Items.Remove(drop_item);
+		if(this.retrieveSlot(drop_item.Slot, out slot)) {
+			if(!being_stolen || (m_CanBeStolenFrom && drop_item.CanBeStolen)) {
+				if(slot.Drop(drop_item)) {
+					drop_item.Owner = null;
+					drop_item.transform.SetParent(null);
 
-			return true;
+					return true;
+				}
+			}
 		}
 
 		return false;
--- Assets/scripts/items/Item.cs
+++ Assets/scripts/items/Item.cs
@@ -1,9 +1,10 @@
 using System.Collections;
+using System.Collections.Generic;
 ﻿using UnityEngine;
 
 public class Item : MonoBehaviour
 {
-	public bool IsOwned {
+	public bool hasOwner {
 		get {
 			return Owner != null;
 		}
@@ -12,7 +13,44 @@ public class Item : MonoBehaviour
 	public Inventory Owner {
 		get { return m_Owner; }
 		set {
+			if(m_Owner != value) {
+				isVisible = (value == null);
+				isPhysical = (value == null);
+			}
+
 			m_Owner = value;
+		}
+	}
+
+	public bool isVisible {
+		get { return m_isVisible; }
+		set {
+			if(m_isVisible != value) {
+				Debug.Log("Changing visibility to: " + value);
+				foreach(Renderer rend in m_Renderers) {
+					rend.enabled = value;
+				}
+			}
+
+			m_isVisible = value;
+		}
+	}
+
+	public bool isPhysical {
+		get { return m_isPhysical; }
+		set {
+			if(m_isPhysical != value) {
+				Debug.Log("Changing physicality to: " + value);
+				if(m_Body) {
+					m_Body.isKinematic = !value;
+				}
+
+				foreach(Collider coll in m_Colliders) {
+					coll.enabled = value;
+				}
+			}
+
+			m_isPhysical = value;
 		}
 	}
 
@@ -23,4 +61,19 @@ public class Item : MonoBehaviour
 	public string Name = "Item";
 	public string Description = "You can hold it!";
 	public string Slot = "general";
+
+	protected bool m_isVisible = true;
+	protected bool m_isPhysical = true;
+	protected Rigidbody m_Body;
+	protected List<Renderer> m_Renderers;
+	protected List<Collider> m_Colliders;
+
+	public void Awake()
+	{
+		m_Body = GetComponent<Rigidbody>();
+		m_Renderers = new List<Renderer>(GetComponentsInChildren<Renderer>());
+		m_Colliders = new List<Collider>(GetComponentsInChildren<Collider>());
+
+		Debug.Log(m_Body);
+	}
 }
