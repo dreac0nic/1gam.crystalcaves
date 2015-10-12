93fa780bbf2980cd996fefe4df87f5e141348d60
SpenSer Bray <matthewqa@gmail.com>
2015-10-11 17:32:58 -0700

    Add base Impulser class to be expanded on by other impulsers.

---
+++ Assets/scripts/utility/Impulser.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+﻿using UnityEngine;
+
+public class Impulser : MonoBehaviour
+{
+	public enum ImpulseType {
+		ATTACK, ALT_ATTACK, REFRESH,
+		PICKUP_ITEM, DROP_ITEM, CYCLE_NEXT_ITEM, CYCLE_PREV_ITEM, SWAP_LAST_USED_ITEM
+	};
+
+	public virtual void Impulse(ImpulseType type)
+	{
+		if(Debug.isDebugBuild) {
+			Debug.Log("Impulse received on " + this.gameObject.name + ": " + type.ToString());
+		}
+	}
+}
