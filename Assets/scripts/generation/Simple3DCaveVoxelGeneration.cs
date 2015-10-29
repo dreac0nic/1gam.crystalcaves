76db59af433cc8bbfc649c7f6956b3624421f58b
SpenSer Bray <matthewqa@gmail.com>
2015-10-28 17:29:34 -0700

    Create 3D version of the automata algorithm.

--- Assets/scripts/generation/Simple3DCaveVoxelGeneration.cs
+++ Assets/scripts/generation/Simple3DCaveVoxelGeneration.cs
@@ -6,14 +6,25 @@ public class Simple3DCaveVoxelGeneration : MonoBehaviour
 {
 	public int ResolutionWidth = 100;
 	public int ResolutionHeight = 120;
+	public int ResolutionDepth = 100;
 	[Range(0, 100)] public float InitialFillPercent = 50.0f;
 	[Range(0, 10)] public int SmoothingPasses = 4;
 
 	public bool CustomSeed = false;
 	public string Seed;
 
+	[Header("DEBUG CONTROLS")]
+	public bool DrawMapGizmos = true;
+	public bool DrawWireframeCubes = false;
+	public bool OnlyDrawActiveCubes = true;
+	public bool ColorCubesWithPosition = false;
+	public bool OnlyColorWithHeight = true;
+	public bool OnlyDrawHighlightedLayer = true;
+	public int HighlightedLayer = 0;
+	public int LayerDrawCap = 0;
+
 	protected System.Random m_RNG;
-	protected bool[,] m_Map;
+	protected bool[,,] m_Map;
 
 	public void Awake()
 	{
@@ -29,13 +40,52 @@ public class Simple3DCaveVoxelGeneration : MonoBehaviour
 
 	public void OnDrawGizmos()
 	{
-		if(m_Map != null) {
-			for(int x = 0; x < m_Map.GetLength(0); ++x) {
-				for(int y = 0; y < m_Map.GetLength(1); ++y) {
-					Gizmos.color = (m_Map[x, y] ? Color.black : Color.white);
-					Vector3 position = new Vector3(-ResolutionWidth/2.0f + x, -ResolutionHeight/2.0f + y);
-					Gizmos.DrawCube(this.transform.position + position, Vector3.one);
+		if(m_Map != null && DrawMapGizmos) {
+			if(OnlyDrawHighlightedLayer) {
+				for(int x = 0; x < m_Map.GetLength(0); ++x) {
+					for(int z = 0; z < m_Map.GetLength(2); ++z) {
+						HighlightedLayer = (HighlightedLayer < 0 ? 0 : HighlightedLayer >= m_Map.GetLength(1) ? m_Map.GetLength(1) - 1 : HighlightedLayer);
+						drawMapGizmo(x, HighlightedLayer, z);
+					}
 				}
+			} else {
+				for(int x = 0; x < m_Map.GetLength(0); ++x) {
+					for(int y = 0; y < m_Map.GetLength(1); ++y) {
+						for(int z = 0; z < m_Map.GetLength(2); ++z) {
+							if(LayerDrawCap <= 0 || y <= LayerDrawCap) {
+								drawMapGizmo(x, y, z);
+							}
+						}
+					}
+				}
+			}
+		}
+	}
+
+	protected void drawMapGizmo(int x, int y, int z)
+	{
+		Vector3 cube_position = new Vector3(-ResolutionWidth/2.0f + 0.5f + x, -ResolutionHeight/2.0f + 0.5f + y, -ResolutionDepth/2.0f + 0.5f + z);
+
+		if(ColorCubesWithPosition) {
+			Color gizmo_color = Color.red*((float)x/ResolutionWidth) + Color.green*((float)y/ResolutionHeight) + Color.blue*((float)z/ResolutionDepth);
+
+			if(OnlyColorWithHeight) {
+				gizmo_color.r = 0.0f;
+				gizmo_color.b = 0.0f;
+			}
+
+			gizmo_color.a = 1.0f;
+
+			Gizmos.color = gizmo_color;
+		} else {
+			Gizmos.color = m_Map[x, y, z] ? Color.black : Color.white;
+		}
+
+		if(!OnlyDrawActiveCubes || m_Map[x, y, z]) {
+			if(DrawWireframeCubes) {
+				Gizmos.DrawWireCube(this.transform.position + cube_position, Vector3.one);
+			} else {
+				Gizmos.DrawCube(this.transform.position + cube_position, Vector3.one);
 			}
 		}
 	}
@@ -56,7 +106,7 @@ public class Simple3DCaveVoxelGeneration : MonoBehaviour
 		}
 
 		m_RNG = new System.Random(Seed.GetHashCode());
-		m_Map = new bool[ResolutionWidth, ResolutionHeight];
+		m_Map = new bool[ResolutionWidth, ResolutionHeight, ResolutionDepth];
 
 		seedMap();
 		smoothMap(SmoothingPasses);
@@ -66,10 +116,12 @@ public class Simple3DCaveVoxelGeneration : MonoBehaviour
 	{
 		for(int x = 0; x < m_Map.GetLength(0); ++x) {
 			for(int y = 0; y < m_Map.GetLength(1); ++y) {
-				if(x <= 0 || x >= ResolutionWidth - 1 || y <= 0 || y >= ResolutionHeight - 1) {
-					m_Map[x, y] = true;
-				} else {
-					m_Map[x, y] = (m_RNG.Next(0,100) < InitialFillPercent) ? true : false;
+				for(int z = 0; z < m_Map.GetLength(2); ++z) {
+					if(x <= 0 || x >= ResolutionWidth - 1 || y <= 0 || y >= ResolutionHeight - 1 || z <= 0 || z >= ResolutionDepth - 1) {
+						m_Map[x, y, z] = true;
+					} else {
+						m_Map[x, y, z] = (m_RNG.Next(0,100) < InitialFillPercent) ? true : false;
+					}
 				}
 			}
 		}
@@ -80,36 +132,40 @@ public class Simple3DCaveVoxelGeneration : MonoBehaviour
 		for(int pass = 0; pass < passes; ++pass) {
 			for(int x = 0; x < ResolutionWidth; ++x) {
 				for(int y = 0; y < ResolutionHeight; ++y) {
-					int neighbor_wallcount = countActiveNeighbors(x, y);
+					for(int z = 0; z < ResolutionDepth; ++z) {
+						int neighbor_wallcount = countActiveNeighbors(x, y, z);
 
-					//*
-					if(neighbor_wallcount > 4) {
-						m_Map[x, y] = true;
-					} else if(neighbor_wallcount < 4) {
-						m_Map[x, y] = false;
-					}
-					//*/
+						//*
+						if(neighbor_wallcount > 15) {
+							m_Map[x, y, z] = true;
+						} else if(neighbor_wallcount < 13) {
+							m_Map[x, y, z] = false;
+						}
+						//*/
 
-					/* Game of Life! :D
-					if(!m_Map[x, y] && neighbor_wallcount == 3) {
-						m_Map[x, y] = true;
-					} else if(neighbor_wallcount <= 1 || neighbor_wallcount >= 4) {
-						m_Map[x, y] = false;
+						/* Game of Life! :D
+						if(!m_Map[x, y, z] && neighbor_wallcount == 3) {
+							m_Map[x, y, z] = true;
+						} else if(neighbor_wallcount <= 1 || neighbor_wallcount >= 4) {
+							m_Map[x, y, z] = false;
+						}
+						//*/
 					}
-					//*/
 				}
 			}
 		}
 	}
 
-	protected int countActiveNeighbors(int x, int y)
+	protected int countActiveNeighbors(int x, int y, int z)
 	{
 		int wall_count = 0;
 
 		for(int neighbor_x = x - 1; neighbor_x <= x + 1; ++neighbor_x) {
 			for(int neighbor_y = y - 1; neighbor_y <= y + 1; ++neighbor_y) {
-				if(neighbor_x < 0 || neighbor_x >= ResolutionWidth || neighbor_y < 0 || neighbor_y >= ResolutionHeight || (!(neighbor_x == x && neighbor_y == y) && m_Map[neighbor_x, neighbor_y])) {
-					++wall_count;
+				for(int neighbor_z = z - 1; neighbor_z <= z + 1; ++neighbor_z) {
+					if(neighbor_x < 0 || neighbor_x >= ResolutionWidth || neighbor_y < 0 || neighbor_y >= ResolutionHeight || neighbor_z < 0 || neighbor_z >= ResolutionDepth || (!(neighbor_x == x && neighbor_y == y && neighbor_z == z) && m_Map[neighbor_x, neighbor_y, neighbor_z])) {
+						++wall_count;
+					}
 				}
 			}
 		}
